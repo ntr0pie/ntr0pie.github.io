807bb4e1c466660a96f7b08700b6bfedf1356214
ntr0pie <tripathi.shreeshv@gmail.com>
2024-12-24 18:49:05 +0530

    gh-ages auto deploy

---
+++ _app/immutable/chunks/2.185fa796.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.b7ea092b.js";export{t as component};
---
+++ _app/immutable/chunks/4.2b9bd542.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/work-page.svelte.8f966b7e.js";export{t as component};
