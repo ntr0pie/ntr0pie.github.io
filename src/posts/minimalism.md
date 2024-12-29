81cc7b03bbaacf12956647c03365b3204eb41fe4
ntr0pie <tripathi.shreeshv@gmail.com>
2024-12-30 00:09:31 +0530

    update post component, add posts

---
+++ src/posts/minimalism.md
@@ -0,0 +1,8 @@
+---
+title: Minimalism
+date:
+image: https://api.time.com/wp-content/uploads/2011/10/111017130018-bw1.jpg?quality=75&w=1440
+subimages: []
+---
+
+"All you needed was a light, a cup of tea, and your stereo." -- Steve Jobs
--- src/routes/stream/Post.svelte
+++ src/routes/stream/Post.svelte
@@ -25,16 +25,20 @@
 
 <!-- Title -->
 <h3 class="text-black text-xl font-semibold mb-2">
-  <span class="mr-1">{data.title}</span>
-  <small class="whitespace-nowrap text-neutral-500 text-base font-normal">
-    {formatTime("%B %Y", data.date)}
-  </small>
+  {#if data.title}
+    <span class="mr-1">{data.title}</span>
+  {/if}
+  {#if data.date}
+    <small class="whitespace-nowrap text-neutral-500 text-base font-normal">
+      {formatTime("%d %B %Y", data.date)}
+    </small>
+  {/if}
 </h3>
 
 <!-- Description and image -->
 <div class="">
-  <div class="">
-    <!-- Image -->
+  <!-- Image -->
+  {#if data.image}
     <div class="col-span-3 md:col-span-1">
       <a rel="external" href={images[`../../posts/${data.image}`]?.default}>
         <img
@@ -46,29 +50,29 @@
         />
       </a>
     </div>
+  {/if}
 
-    <!-- Sub-images -->
-    {#if data.subimages}
-      <div class="grid grid-cols-3 gap-4 md:gap-8 lg:gap-12">
-        {#each data.subimages as image}
-          <div class="col-span-full md:col-span-1">
-            <a rel="external" href={images[`../../posts/${image}`]?.default}>
-              <img
-                src={isUrl(image)
-                  ? image
-                  : images[`../../posts/${data.image}`]?.default}
-                alt="{data.title} subimage"
-              />
-            </a>
-          </div>
-        {/each}
-      </div>
-    {/if}
-
-    <!-- Description -->
-    <div class="col-span-3 md:col-span-2">
-      <Markdown source={data.content} />
+  <!-- Sub-images -->
+  {#if data.subimages}
+    <div class="grid grid-cols-3 gap-4 md:gap-8 lg:gap-12">
+      {#each data.subimages as image}
+        <div class="col-span-full md:col-span-1">
+          <a rel="external" href={images[`../../posts/${image}`]?.default}>
+            <img
+              src={isUrl(image)
+                ? image
+                : images[`../../posts/${data.image}`]?.default}
+              alt="{data.title} subimage"
+            />
+          </a>
+        </div>
+      {/each}
     </div>
+  {/if}
+
+  <!-- Description -->
+  <div class="col-span-3 md:col-span-2">
+    <Markdown source={data.content} />
   </div>
 </div>
 
